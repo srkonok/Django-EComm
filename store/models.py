616165776c719d3d64e6831f8b520671b274810f
shah.konok <shahriar.konok@shadhinlab.com>
2022-12-26 16:08:18 +0600

    model creation

---
+++ store/models.py
@@ -0,0 +1,50 @@
+from django.db import models
+
+# Create your models here.
+class Collection(models.Model):
+    title=models.CharField(max_length=255)
+
+
+class Product (models.Model):
+    title= models.CharField(max_length=255)
+    description=models.CharField()
+    price= models.DecimalField(max_digits=6,decimal_places=2)
+    inventory=models.IntegerField()
+    last_update=models.DateTimeField(auto_now=True)
+    collection=models.ForeignKey(Collection,on_delete=models. )
+
+class Customer(models.Model):
+    MEMBERSHIP_BRONZE='B',
+    MEMBERSHIP_SILVER='S',
+    MEMBERSHIP_GOLD='G'
+
+    MEMBERSHIP_CHOICES=[
+        (MEMBERSHIP_BRONZE,'Bronze'),
+        (MEMBERSHIP_SILVER,'Silver'),
+        (MEMBERSHIP_GOLD,'Gold')
+    ]
+
+    first_name=models.CharField(max_length=255)
+    last_name=models.CharField(max_length=255)
+    email=models.EmailField(unique=True)
+    phone=models.CharField(max_length=255)
+    birth_date=models.DateField(null=True)
+    membership= models.CharField(max_length=1,choices=MEMBERSHIP_CHOICES,default=MEMBERSHIP_BRONZE)
+
+class Order(models.Model):
+    PAYMENT_STATUS_PENDING='P',
+    PAYMENT_STATUS_COMPLETE='C',
+    PAYMENT_STATUS_FAILED='F'
+
+    PAYMENT_STATUS_CHOICE=[
+        (PAYMENT_STATUS_PENDING,'Pending'),
+        (PAYMENT_STATUS_COMPLETE,'Complete'),
+        (PAYMENT_STATUS_FAILED,'Failed')
+    ]
+    placed_at=models.DateTimeField(auto_now_add=True)
+    payment_status=models.CharField(max_length=1,choices=PAYMENT_STATUS_CHOICE,default=PAYMENT_STATUS_PENDING)
+
+class Address(models.Model):
+    street=models.CharField(max_length=255)
+    city= models.CharField(max_length=255)
+    customer= models.ForeignKey(Customer,on_delete=models.CASCADE)
